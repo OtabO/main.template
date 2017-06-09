972111fc08c9a93890c043af61e11757a248fb18
zhangsx <zhangshixin@shhxzq.com>
2017-06-09 15:34:04 +0800

    do

---
+++ bean.template/src/main/java/bean/template/RolePermissionDO.java
@@ -0,0 +1,9 @@
+package bean.template;
+
+import java.io.Serializable;
+
+/**
+ * Created by zhangsx on 2017/6/9.
+ */
+public class RolePermissionDO implements Serializable {
+}
--- bean.template/src/main/java/bean/template/UserDO.java
+++ bean.template/src/main/java/bean/template/UserDO.java
@@ -1,12 +1,54 @@
 package bean.template;
 
+import java.io.Serializable;
+
 /**
  * Created by zhangsx on 2017/6/9.
  */
-public class UserDO {
+public class UserDO implements Serializable{
     private long userID;
     private String userName;
     private String pwdMD5;
     private String email;
     private String phone;
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(long userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwdMD5() {
+        return pwdMD5;
+    }
+
+    public void setPwdMD5(String pwdMD5) {
+        this.pwdMD5 = pwdMD5;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
